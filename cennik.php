8e0cfd519a8eabac935d0ef89911028a16d58157
Przemek Jedlikowski <przemek@jedlikowski.com>
2024-01-09 20:30:31 +0100

    część 1 Przemka

---
+++ cennik.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wynajem pokoi</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<body>
+    <header>
+        <h1>Pensjonat pod dobrym humorem</h1>
+    </header>
+    <section>
+        <aside class="lewy">
+            <a href="index.html">GŁÓWNA</a>
+            <img src="1szary.jpg" alt="pokoje">
+        </aside>
+        <main>
+            <a href="cennik.php">CENNIK</a>
+            <table>
+                <tr>
+                    <td>1</td>
+                    <td>ileś-osobowy</td>
+                    <td>50</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ileś-osobowy</td>
+                    <td>50</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>ileś-osobowy</td>
+                    <td>50</td>
+                </tr>
+            </table>
+        </main>
+        <aside class="prawy">
+            <a href="kalkulator.html">KALKULATOR</a>
+            <img src="3szary.jpg" alt="pokoje">
+        </aside>
+    </section>
+    <footer>
+        Stronę opracował: Adam Karczewski i Przemek Jedlikowski, TEB Wrocław
+    </footer>
+</body>
+</html>
